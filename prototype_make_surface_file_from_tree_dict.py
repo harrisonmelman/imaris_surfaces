72892d535a428fd36b3c007c89176ee5bf0decd8
Harrison Mansour <harrisonmelman@users.noreply.github.com>
2025-01-17 11:08:41 -0500

    put main code into main function

--- prototype_make_surface_file_from_tree_dict.py
+++ prototype_make_surface_file_from_tree_dict.py
@@ -48,7 +48,7 @@ def create_surface(node):
 
 
 # node is a Dict entry in the RCCF label tree
-# parent_group is an imaris group object (a folder)
+# parent_group is an Imaris group object (a folder)
 def traverse(node, parent_group):
     if node is None:
         return
@@ -79,60 +79,60 @@ def traverse(node, parent_group):
     return
 
 
-# MAIN
-# if you want this script to handle imaris launch and data load
-# if false, will only sea4rch for application 101 and load image 0 from the scene. be careful.
-open_imaris = False
+if __name__ == "__main__":
+    # if you want this script to handle imaris launch and data load
+    # if false, will only search for application 101 and load image 0 from the scene. be careful.
+    open_imaris = False
 
-# whole brain root
-root_structure_id = 997
-output_dir = "B:/ProjectSpace/hmm56/imaris_surfaces/test_results"
-RCCF_tree_file = "B:/ProjectSpace/hmm56/imaris_surfaces/RCCF_tree.pkl"
-RCCF_csv_file = "K:/workstation/static_data/atlas/symmetric15um/labels/RCCF/symmetric15um_RCCF_labels_lookup.txt"
-label_imaris_path = r"B:\22.gaj.49\DMBA\Aligned-Data\Other\ims\labels\RCCF\DMBA_RCCF_labels.ims"
-RCCF_label_colors = imsurf.read_csv_into_memory(RCCF_csv_file)
+    # whole brain root
+    root_structure_id = 997
+    output_dir = "B:/ProjectSpace/hmm56/imaris_surfaces/test_results/2025"
+    RCCF_tree_file = "B:/ProjectSpace/hmm56/imaris_surfaces/RCCF_tree-2025.pkl"
+    RCCF_csv_file = "K:/workstation/static_data/atlas/symmetric15um/labels/RCCF/symmetric15um_RCCF_labels_lookup.txt"
+    label_imaris_path = r"B:\22.gaj.49\DMBA\Aligned-Data\Other\ims\labels\RCCF\DMBA_RCCF_labels.ims"
+    RCCF_label_colors = imsurf.read_csv_into_memory(RCCF_csv_file)
 
-# launch imaris
-# old version
-# imaris_path = r"C:\Program Files\Bitplane\Imaris 10.1.1\Imaris.exe"
-# xt_path = r"C:\Program Files\Bitplane\Imaris 10.1.1\XT\python3"
-imaris_path = r"C:/Program Files/Bitplane/Imaris 10.1.1/Imaris.exe"
-xt_path = r"C:/Program Files/Bitplane/Imaris 10.1.1/XT/python3"
-imaris_path = r"C:/Program Files/Bitplane/Imaris 10.1.1/Imaris.exe"
-xt_path = 'C:\\Program Files\\Bitplane\\Imaris 10.1.1\\XT\\python3'
+    # launch imaris
+    # old version
+    # imaris_path = r"C:\Program Files\Bitplane\Imaris 10.1.1\Imaris.exe"
+    # xt_path = r"C:\Program Files\Bitplane\Imaris 10.1.1\XT\python3"
+    imaris_path = r"C:/Program Files/Bitplane/Imaris 10.1.1/Imaris.exe"
+    xt_path = r"C:/Program Files/Bitplane/Imaris 10.1.1/XT/python3"
+    imaris_path = r"C:/Program Files/Bitplane/Imaris 10.1.1/Imaris.exe"
+    xt_path = 'C:\\Program Files\\Bitplane\\Imaris 10.1.1\\XT\\python3'
 
-if open_imaris:
-    imaris_process = subprocess.Popen([imaris_path, "id101"])
-    # give imaris enough time to open
-    time.sleep(10)
+    if open_imaris:
+        imaris_process = subprocess.Popen([imaris_path, "id101"])
+        # give imaris enough time to open
+        time.sleep(10)
 
-# TODO: maybe put this whole bundle of code into a module function. returns imaris_[lib,server,app]
-# setup imaris connection
-# these append statements are required to correctly find ImarisLib and all of its dependencies
-# this sys.path.append is the correct way to modify your PYTHONPATH variable
-sys.path.append(imaris_path)
-sys.path.append(xt_path)
-sys.path.insert(0, "k:/workstation/code/shared/pipeline_utilities/imaris")
-# to fix DLL load failure. only loads from trusted sources.
-# probably will not work since pthon < 3.8
-import ImarisLib
+    # TODO: maybe put this whole bundle of code into a module function. returns imaris_[lib,server,app]
+    # setup imaris connection
+    # these append statements are required to correctly find ImarisLib and all of its dependencies
+    # this sys.path.append is the correct way to modify your PYTHONPATH variable
+    sys.path.append(imaris_path)
+    sys.path.append(xt_path)
+    sys.path.insert(0, "k:/workstation/code/shared/pipeline_utilities/imaris")
+    # to fix DLL load failure. only loads from trusted sources.
+    # probably will not work since python < 3.8
+    import ImarisLib
 
-imaris_lib = ImarisLib.ImarisLib()
-imaris_server = imaris_lib.GetServer()
-# previously v
-imaris_app = imaris_lib.GetApplication(101)
-factory = imaris_app.GetFactory()
-# load label file programatically
-load_options = ""
-if open_imaris:
-    imaris_app.FileOpen(label_imaris_path, load_options)
-# load only the label file so that this is unambiguous
-img = imaris_app.GetImage(0)
+    imaris_lib = ImarisLib.ImarisLib()
+    imaris_server = imaris_lib.GetServer()
+    # previously v
+    imaris_app = imaris_lib.GetApplication(101)
+    factory = imaris_app.GetFactory()
+    # load label file programatically
+    load_options = ""
+    if open_imaris:
+        imaris_app.FileOpen(label_imaris_path, load_options)
+    # load only the label file so that this is unambiguous
+    img = imaris_app.GetImage(0)
 
-from pprint import pprint
+    from pprint import pprint
 
-with open(RCCF_tree_file, "rb") as f:
-    RCCF_tree = pickle.load(f)
-    root = RCCF_tree[root_structure_id]
-    # the parent of the root node is the root of the imaris filesystem
-    traverse(root, imaris_app.GetSurpassScene())
+    with open(RCCF_tree_file, "rb") as f:
+        RCCF_tree = pickle.load(f)
+        root = RCCF_tree[root_structure_id]
+        # the parent of the root node is the root of the imaris filesystem
+        traverse(root, imaris_app.GetSurpassScene())
