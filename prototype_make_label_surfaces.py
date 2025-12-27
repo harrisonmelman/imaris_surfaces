094dd148fdc74603bd93d1c03055256bfe14a332
Harrison Mansour <harrisonmelman@users.noreply.github.com>
2025-12-27 09:46:12 -0500

    commit hanging changes before modifying (need to update to work on imaris 11)

--- prototype_make_label_surfaces.py
+++ prototype_make_label_surfaces.py
@@ -10,93 +10,101 @@ import random
 import nrrd
 import imagej
 import csv
-import imaris_surface_helpers
-
-# these append statements are required to correctly find ImarisLib and all of its dependencies
-# this sys.path.append is the correct way to modify your PYTHONPATH variable
-sys.path.append(r'C:\Program Files\Bitplane\Imaris 9.9.0\XT\python3'.replace(r'\\', '/'))
-sys.path.insert(0, 'k:/workstation/code/shared/pipeline_utilities/imaris')
-# this is all of a sudden giving me a DLL load failre
-# maybe it is a shortcoming of pycharm console??
-# no module. wrong py version?
-# import ImarisLib
+import imaris_surface_helpers as imsurf
 
 
-########### MAIN ###########
-RCCF_tree_file = "B:/ProjectSpace/hmm56/imaris_surfaces/RCCF_tree.pkl"
-with open(RCCF_tree_file, 'wb') as f:
-    RCCF_tree = pickle.load(f)
+if __name__ == "__main__":
+    # if you want this script to handle imaris launch and data load
+    # if false, will only search for application 101 and load image 0 from the scene. be careful.
+    open_imaris = False
 
-output_dir = "B:/ProjectSpace/hmm56/imaris_surfaces/test_results"
-# TODO change to symmnetric15 labels, DMBA labels are out of date
-RCCF_csv_file = "K:/workstation/static_data/atlas/symmetric15um/labels/RCCF/symmetric15um_RCCF_labels_lookup.txt"
-# RCCF_csv_file = "B:/22.gaj.49/DMBA/Aligned-Data-RAS/labels/RCCF/DMBA_RCCF_labels_lookup.txt"
-RCCF_label_colors = imaris_surface_helpers.read_csv_into_memory(RCCF_csv_file)
-# sys.exit("testing")
+    output_dir = "B:/ProjectSpace/hmm56/imaris_surfaces/test_results-0.3"
+    label_imaris_path = "B:/22.gaj.49/DMBA/Aligned-Data-0.3/Other/ims/labels/RCCF/DMBA_RCCF_labels.ims"
+    RCCF_csv_file = "K:/workstation/static_data/atlas/symmetric15um/labels/RCCF/symmetric15um_RCCF_labels_lookup.txt"
+    RCCF_label_colors = imsurf.read_csv_into_memory(RCCF_csv_file)
 
-label_indices = list(range(1, 181, 30)) + list(range(1001, 1181, 30))
+    # if this fails, did you run bash environment_setup.bash?
+    # path setup and import of ImarisLib
+    # this is specifically adding on to the pyrthon path, not the bash path (which is what environment_setup.bash modifies)
+    sys.path.append(r'C:\Program Files\Bitplane\Imaris 10.1.1\XT\python3'.replace(r'\\', '/'))
+    sys.path.insert(0, 'k:/workstation/code/shared/pipeline_utilities/imaris')
+    import ImarisLib
 
-# TODO: loading data into imaris is failing now. double check what I did for NeuronCounting
-# launch imaris
-imaris_process = subprocess.Popen(['C:\\Program Files\\Bitplane\\Imaris 10.1.1\\Imaris.exe', 'id101'])
-# give imaris enough time to open
-time.sleep(10)
 
-# find the imaris application to interact with it (opened by the setup script)
-vServer = imaris_surface_helpers.GetServer()
-vImarisLib = ImarisLib.ImarisLib()
-v = vImarisLib.GetApplication(101)
+    label_indices = list(range(1, 181, 30)) + list(range(1001, 1181, 30))
 
-# probably not necessary for this task
-# ij = imagej.init(r'K:\CIVM_Apps\Fiji.app', mode='interactive')
+    # TODO: loading data into imaris is failing now. double check what I did for NeuronCounting
+    # launch imaris
+    imaris_process = subprocess.Popen(['C:\\Program Files\\Bitplane\\Imaris 10.1.1\\Imaris.exe', 'id101'])
+    #imaris_process = subprocess.Popen(['C:\\Program Files\\Bitplane\\Imaris 9.9.0\\Imaris.exe', 'id101'])
+    # give imaris enough time to open
+    time.sleep(10)
 
-# input data
-label_imaris_path = "B:\22.gaj.49\DMBA\Aligned-Data\Other\ims\labels\RCCF\DMBA_RCCF_labels.ims"
+    # find the imaris application to interact with it (opened by the setup script)
+    """
+    why did i ever make this GetServer function? makes no sense currently...
+    def GetServer():
+        imaris_lib = ImarisLib.ImarisLib()
+        imaris_server = imaris_lib.GetServer()
+        return imaris_server
+    """
+    #vServer = imsurf.GetServer()
+    vImarisLib = ImarisLib.ImarisLib()
+    vServer = vImarisLib.GetServer()
+    v = vImarisLib.GetApplication(101)
 
-# load data into imaris
-load_options = ""
-# v.FileOpen(label_imaris_path, load_options)
+    # probably not necessary for this task
+    # ij = imagej.init(r'K:\CIVM_Apps\Fiji.app', mode='interactive')
 
-# load only the label volume. Then, volume 0 is the labelset
-img = v.GetImage(0)
-vExtentMin = [img.GetExtendMinX(), img.GetExtendMinY(), img.GetExtendMinZ()]
-vExtentMax = [img.GetExtendMaxX(), img.GetExtendMaxY(), img.GetExtendMaxZ()]
-print('vExtentMin:', vExtentMin)
-aChannel = 0
+    # input data
+    label_imaris_path = "B:\22.gaj.49\DMBA\Aligned-Data-0.3\Other\ims\labels\RCCF\DMBA_RCCF_labels.ims"
 
-label_indices = [1, 15, 30, 90, 145]
-# 0x means hex number
-# but hex gives an error
-# values in 0-255 give good values
-colors = [0xFFFF00FF, 0xFF00FFFF, 0xFF0000FF, 0xFFFF0000, 0xFF00FF00]
-# colors = [0, 230, 45, 145, 255]
-i = 0
-# generate a surface for each label
-for x in label_indices:
-    label_bounds = [x - 0.5, x + 0.5]
-    vSur = v.GetImageProcessing().DetectSurfacesWithUpperThreshold(img, [], aChannel, 0, 0, True, False,
-                                                                   label_bounds[0], True, False, label_bounds[1], None)
-    vSur.SetName('Surface ' + str(x))
-    # color = colors[i]
+    # load data into imaris
+    load_options = ""
+    #v.FileOpen(label_imaris_path, load_options)
+    #print("sleep 5 to wait for file load")
+    #time.sleep(5)
 
-    color = imaris_surface_helpers.get_color_from_RCCF_csv(RCCF_label_colors, x)
-    # aValue consists in four bytes encoding the red (least significant byte), green, blue and alpha (most significant byte) components of the color to be set.
-    # The range of each component is 0-255. An alpha value of 0 represents full opacity, while 255 means full transparency.
-    # vSur.SetColorRGBA(color)
-    color = imaris_surface_helpers.get_random_color()
-    gotten_color = vSur.GetColorRGBA()
-    print("working on ROI: {}".format(x))
-    print("got color: {}".format(gotten_color))
-    print("setting color to: {}".format(color))
+    # load only the label volume. Then, volume 0 is the labelset
+    img = v.GetImage(0)
+    vExtentMin = [img.GetExtendMinX(), img.GetExtendMinY(), img.GetExtendMinZ()]
+    vExtentMax = [img.GetExtendMaxX(), img.GetExtendMaxY(), img.GetExtendMaxZ()]
+    print('vExtentMin:', vExtentMin)
+    aChannel = 0
 
-    vSur.SetColorRGBA(color)
-    v.GetSurpassScene().AddChild(vSur, 1)
-    i = i + 1
+    label_indices = [1, 15, 30, 90, 145]
+    # 0x means hex number
+    # but hex gives an error
+    # values in 0-255 give good values
+    colors = [0xFFFF00FF, 0xFF00FFFF, 0xFF0000FF, 0xFFFF0000, 0xFF00FF00]
+    # colors = [0, 230, 45, 145, 255]
+    i = 0
+    # generate a surface for each label
+    for x in label_indices:
+        label_bounds = [x - 0.5, x + 0.5]
+        vSur = v.GetImageProcessing().DetectSurfacesWithUpperThreshold(img, [], aChannel, 0, 0, True, False,
+                                                                    label_bounds[0], True, False, label_bounds[1], None)
+        vSur.SetName('Surface ' + str(x))
+        # color = colors[i]
 
-"""factory = v.GetFactory()
-# create new folder (generic name)
-container=factory.CreateDataContainer()
-# add folder to scene
-v.GetSurpassScene().AddChild(container)
-# add a surface to the new container
-container.AddChild(surface,-1)"""
+        color = imsurf.get_color_from_RCCF_csv(RCCF_label_colors, x)
+        # aValue consists in four bytes encoding the red (least significant byte), green, blue and alpha (most significant byte) components of the color to be set.
+        # The range of each component is 0-255. An alpha value of 0 represents full opacity, while 255 means full transparency.
+        # vSur.SetColorRGBA(color)
+        color = imsurf.get_random_color()
+        gotten_color = vSur.GetColorRGBA()
+        print("working on ROI: {}".format(x))
+        print("got color: {}".format(gotten_color))
+        print("setting color to: {}".format(color))
+
+        vSur.SetColorRGBA(color)
+        v.GetSurpassScene().AddChild(vSur, 1)
+        i = i + 1
+
+    """factory = v.GetFactory()
+    # create new folder (generic name)
+    container=factory.CreateDataContainer()
+    # add folder to scene
+    v.GetSurpassScene().AddChild(container)
+    # add a surface to the new container
+    container.AddChild(surface,-1)"""
--- prototype_make_surface_file_from_tree_dict.py
+++ prototype_make_surface_file_from_tree_dict.py
@@ -58,7 +58,7 @@ def check_if_dead_end(node):
     # recursively check children to see if we end up with a real ROI
     # if we do not, then we should skip this branch without making a folder
     # only if there are NO real regions in that ENTIRE BRANCH
-    logger.debug("CHECK IF DEAD END was passed node: %s", node)
+    #logger.debug("CHECK IF DEAD END was passed node: %s", node)
     if node["ROI_num"] == "NaN":
         # then we are some type of folder
         if not node["children"]:
@@ -69,10 +69,10 @@ def check_if_dead_end(node):
             for child in node["children"]:
                 # loop through and if ANY branch is nonempty, then we return false
                 if not check_if_dead_end(RCCF_tree[child]):
-                    logger.debug("Descendents here. Not a dead end. Return false")
+                    #logger.debug("Descendents here. Not a dead end. Return false")
                     return False
             # only if we loop trhough all and find no descendents, then do we return True
-            logger.debug("DEAD END")
+            #logger.debug("DEAD END")
             return True
     # if ROI_num is a number, then it is an RCCF region. absolutely not a dead end. 
     return False
@@ -117,15 +117,19 @@ def traverse(node, parent_group):
         return
 
 
-def flat_traverse(node, parent_group, white_list=None):
+def flat_traverse(node,
+                  default_parent_group,
+                  white_list=None,
+                  left_right_split=False,
+                  left_container_for_split=None,
+                  right_container_for_split=None):
     """This approach ignores the hierarchy. Creates a surface object per roi
     This function should save all of the regions to separate files"""
     # basically, it traverses as normally
     # but it always passes the root node as parent_group to prevent hierarchies
     # and it does not create the folder structure
-    # otherwise works exactly the same
     # white list is an optional argument, if True, it will generate ONLY the asked for regions
-    logger.debug("Current node in flat_traverse: %s", node)
+    #logger.debug("passed white_list: %s", white_list) if white_list is not None else logger.debug("white_list is None")
     if node is None:
         logger.debug("Node is None in flat_traverse. Return.")
         return
@@ -134,30 +138,49 @@ def flat_traverse(node, parent_group, white_list=None):
         if node["ROI_num"] == "NaN":
             return
         if white_list is not None:
-            logger.debug("White list is provided for flat_traverse.")
-            if node["ROI_num"] not in white_list:
-                # if we are given an explicit list of children, then check this one is a member
-                logger.debug("ROI %s is not in the white list. Skipping.", node["ROI_num"])
+            try:
+                current_roi_num_int = int(node["ROI_num"])
+                if current_roi_num_int not in white_list:
+                    logger.debug("ROI %s is not in the white list. Skipping.", current_roi_num_int)
+                    return
+                logger.debug("ROI %s is in the white list. Creating surface.", current_roi_num_int)
+            except ValueError:
+                logger.warning("Invalid ROI_num '%s' for node %s while checking whitelist. Skipping.", node["ROI_num"], node["structure_name"])
                 return
+
         logger.info("Explicit ROI found in flat_traverse. Creating surface.")
         logger.info("ROI Details: Number: %s, ID: %s, Name: %s",
                     node["ROI_num"], node["structure_id"], node["structure_name"])
         surface = create_surface(node)
         surface.SetName(node["structure_name"])
-        parent_group.AddChild(surface, -1)
+
+        # Decide where to add the surface
+        if left_right_split and left_container_for_split and right_container_for_split:
+            try:
+                roi_num_int = int(node["ROI_num"])
+                if roi_num_int < 181:  # Condition for "left"
+                    left_container_for_split.AddChild(surface, -1)
+                    logger.info("Added surface '%s' (ROI %s) to LEFT container '%s'", node["structure_name"], roi_num_int, left_container_for_split.GetName())
+                else:
+                    right_container_for_split.AddChild(surface, -1)
+                    logger.info("Added surface '%s' (ROI %s) to RIGHT container '%s'", node["structure_name"], roi_num_int, right_container_for_split.GetName())
+            except ValueError:
+                logger.warning("Could not convert ROI_num '%s' to int for L/R split. Adding to default parent.", node["ROI_num"])
+                default_parent_group.AddChild(surface, -1)
+        else:
+            default_parent_group.AddChild(surface, -1)
+            logger.info("Added surface '%s' (ROI %s) to default parent group '%s'", node["structure_name"], node["ROI_num"], default_parent_group.GetName())
         return
     # else it is a group
     if not check_if_dead_end(node):
-        logger.debug("Node %s is a group and not a dead end (flat_traverse). Keep traversing.", node.get("structure_id", "N/A"))
-        #container = factory.CreateDataContainer()
-        #container.SetName(node["structure_name"])
-        #parent_group.AddChild(container, -1)
         for child in node["children"]:
-            logger.debug("Calling flat_traverse on child: %s", child)
-            flat_traverse(RCCF_tree[child], parent_group)
+            flat_traverse(RCCF_tree[child], default_parent_group,
+                          white_list=white_list, left_right_split=left_right_split,
+                          left_container_for_split=left_container_for_split,
+                          right_container_for_split=right_container_for_split)
         return
     else:
-        logger.debug("Node %s is a dead end or already processed in flat_traverse. Return.", node.get("structure_id", "N/A"))
+        #logger.debug("Node %s is a dead end or already processed in flat_traverse. Return.", node.get("structure_id", "N/A"))
         return
 
 
@@ -167,9 +190,8 @@ def load_white_list(file_path):
     # has an extra column at the end called 'in_manuscript_figure' which is either 'yes' or 'nan'
     df = pd.read_excel(file_path)
     length = df.shape[0]
-    white_list = [df["# ROI"][x] for x in range(length) if df['in_manuscript_figure'][x] == 'yes']
+    white_list = [int(df["# ROI"][x]) for x in range(length) if df['in_manuscript_figure'][x] == 'yes']
     return white_list
-
 
 
 if __name__ == "__main__":
@@ -183,10 +205,12 @@ if __name__ == "__main__":
 
     # if you want this script to handle imaris launch and data load
     # if false, will only search for application 101 and load image 0 from the scene. be careful.
-    open_imaris = True
+    open_imaris = False
+    SPLIT_LEFT_RIGHT = True # Set to True to enable left/right group splitting
 
     white_list_file = "B:/20.5xfad.01/BXD77_paper/imaris_figure/bxd77_5xfadpaper_figure_colors.xlsx"
     white_list = load_white_list(white_list_file)
+    logging.debug("Found white list: %s", white_list) if white_list is not None else logging.debug("white_list not found")
 
     output_dir = "B:/ProjectSpace/hmm56/imaris_surfaces/test_results/2025"
     RCCF_tree_file = "B:/ProjectSpace/hmm56/imaris_surfaces/data/templates/RCCF_tree-reduced.pkl"
@@ -240,8 +264,29 @@ if __name__ == "__main__":
     logger.info("Attempting to open RCCF tree file: %s", RCCF_tree_file)
     with open(RCCF_tree_file, "rb") as f:
         RCCF_tree = pickle.load(f)
-        root = RCCF_tree[root_structure_id]
-        # the parent of the root node is the root of the imaris filesystem
-        #traverse(root, imaris_app.GetSurpassScene())
-        #flat_traverse(RCCF_tree[root["children"][2]], imaris_app.GetSurpassScene())  # test on subset for speed
-        flat_traverse(root, imaris_app.GetSurpassScene(), white_list=white_list)
+
+    root = RCCF_tree[root_structure_id]
+    main_scene_parent = imaris_app.GetSurpassScene()
+
+    left_container = None
+    right_container = None
+
+    if SPLIT_LEFT_RIGHT:
+        container_base_name = root["structure_name"] if root else "RCCF_Split"
+
+        left_container = factory.CreateDataContainer()
+        left_container.SetName(f"{container_base_name} Left")
+        main_scene_parent.AddChild(left_container, -1)
+        logger.info("Created LEFT root container: %s", left_container.GetName())
+
+        right_container = factory.CreateDataContainer()
+        right_container.SetName(f"{container_base_name} Right")
+        main_scene_parent.AddChild(right_container, -1)
+        logger.info("Created RIGHT root container: %s", right_container.GetName())
+
+    flat_traverse(root,
+                  default_parent_group=main_scene_parent,
+                  white_list=white_list,
+                  left_right_split=SPLIT_LEFT_RIGHT,
+                  left_container_for_split=left_container,
+                  right_container_for_split=right_container)
